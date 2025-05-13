a6bd1c64089005eb7f8805f4577c21dd9d08f360
youssefabdelmaged <youssefabdelmaged50@gmail.com>
2025-05-13 20:34:31 +0300

    flip is done

--- script.js
+++ script.js
@@ -12,45 +12,37 @@ const TIME_LIMIT = 120;
 
 //start timer
 function startTimer() {
-    time = TIME_LIMIT;
-    messageElement.style.display = "none";
+  time = TIME_LIMIT;
+  messageElement.style.display = "none";
+  timerElement.textContent = `Time: ${time}s`;
+  timer = setInterval(() => {
+    time--;
     timerElement.textContent = `Time: ${time}s`;
-    timer = setInterval(() => {
-        time--;
-        timerElement.textContent = `Time: ${time}s`;
-        if (time <= 0) {
-            clearInterval(timer);
-            disableDeck = true;
-            messageElement.textContent = "You lose! Time's up.";
-            messageElement.style.display = "block";
-        }
-    }, 1000);
+    if (time <= 0) {
+      clearInterval(timer);
+      disableDeck = true;
+      messageElement.textContent = "You lose! Time's up.";
+      messageElement.style.display = "block";
+    }
+  }, 1000);
 }
 // stop timer
 
 function stopTimer() {
-    clearInterval(timer);
+  clearInterval(timer);
 }
 
-
-
-
-
-
-
-
-
 // flip card
-function flipCard({target: clickedCard}) {
-    if(cardOne !== clickedCard && !disableDeck) {
-        clickedCard.classList.add("flip");
-        if(!cardOne) {
-            return cardOne = clickedCard;
-        }
-        cardTwo = clickedCard;
-        disableDeck = true;
-        let cardOneImg = cardOne.querySelector(".back-view img").src,
-        cardTwoImg = cardTwo.querySelector(".back-view img").src;
-        matchCards(cardOneImg, cardTwoImg);
-    }
+function flipCard({ target: clickedCard }) {
+  if (cardOne !== clickedCard && !disableDeck) {
+    clickedCard.classList.add("flip");
+    if (!cardOne) {
+      return (cardOne = clickedCard);
+    }
+    cardTwo = clickedCard;
+    disableDeck = true;
+    let cardOneImg = cardOne.querySelector(".back-view img").src,
+      cardTwoImg = cardTwo.querySelector(".back-view img").src;
+    matchCards(cardOneImg, cardTwoImg);
+  }
 }
